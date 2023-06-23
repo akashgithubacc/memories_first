33c547eed2cb76c1d2feabe6249bbf4c4040f275
akashgithubacc <rohansingh25december@gmail.com>
2023-06-23 23:48:22 +0530

    Changed the api path to a common path

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,22 +2,22 @@ import axios from "axios";
 
 const url = "http://localhost:3000/posts";
 
-export const fetchPosts = (page) => axios.get(`${url}?page=${page}`);
+export const fetchPosts = (page) => axios.get(`?page=${page}`);
 
-export const createPost = (newPost) => axios.post(url, newPost);
+export const createPost = (newPost) => axios.post("/", newPost);
 
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, updatedPost);
+  axios.patch(`/${id}`, updatedPost);
 
-export const deletePost = (id) => axios.delete(`${url}/${id}`, id);
+export const deletePost = (id) => axios.delete(`/${id}`, id);
 
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`, id);
+export const likePost = (id) => axios.patch(`/${id}/likePost`, id);
 
 export const fetchPostsBySearch = (searchQuery) =>
   axios.get(
-    `${url}/search?searchQuery=${searchQuery.search || "none"}&tags=${
+    `/search?searchQuery=${searchQuery.search || "none"}&tags=${
       searchQuery.tags
     } `
   );
 
-export const fetchPost = (id) => axios.get(`${url}/${id}`);
+export const fetchPost = (id) => axios.get(`/${id}`);
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 dotenv.config();
@@ -24,6 +30,17 @@ const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message)); */
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 const connectDB = async () => {
   try {
