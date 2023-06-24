70038e707365cfb9f437e2ab6b2d1b5232e6aca1
akashgithubacc <rohansingh25december@gmail.com>
2023-06-24 07:27:53 +0530

    changed path in servver indexjs

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 //app.use(cors());
-app.use("/posts", postRoutes);
+app.use("/postss", postRoutes);
 
 const PORT = process.env.PORT || 5000;
 
